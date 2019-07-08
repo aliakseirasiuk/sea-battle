2e2398034bb9ec365f1e5eea854ee4a7ddc917b0
aliaksei.rasiuk@gmail.com <bikuty774>
2019-07-08 22:17:58 +0300

    chapter 5 finished

--- SimpleDotComGame.java
+++ SimpleDotComGame.java
@@ -1,6 +1,24 @@
 public class SimpleDotComGame{
+	
 	public static void main(String[] args) {
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+
 		SimpleDotCom dotCom = new SimpleDotCom();
-		System.out.println(dotCom.getArrSize());
+		int randomNum = (int) (Math.random() * 5);
+
+		int[] locationCells = {randomNum, randomNum + 1, randomNum + 2};
+		dotCom.setLocationCells(locationCells);
+		boolean isAlive = true;
+		
+		while(isAlive){
+			String guess = helper.getUserInput("Enter the number: ");
+			String result = dotCom.checkYourself(guess);
+			numOfGuesses++;
+			if(result.equals("Ship sinked!")){
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " turn(s) to win");
+			}
+		}
 	}
 }
